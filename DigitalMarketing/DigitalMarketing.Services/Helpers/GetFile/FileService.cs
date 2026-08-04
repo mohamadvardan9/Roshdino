617271aabf55a926a32b994dfb306281e967ab33
“Taghi” <mohamadvarda9@gmail.com>
2026-08-05 02:02:23 +0330

    Add IFileService

---
+++ DigitalMarketing/DigitalMarketing.Services/Helpers/GetFile/FileService.cs
@@ -0,0 +1,40 @@
+﻿namespace DigitalMarketing.DigitalMarketing.Services.Helpers.GetFile
+{
+    public class FileService : IFileService
+    {
+        private readonly IWebHostEnvironment _environment;
+        public FileService(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+
+        public async Task<string> UploadImageAsync(IFormFile file, string folderName)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+            string folderPath = Path.Combine(_environment.WebRootPath, "uploads", folderName);
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            string filePath = Path.Combine(folderPath, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return $"/uploads/{folderName}/{fileName}";
+
+
+        }
+    }
+}
---
+++ DigitalMarketing/DigitalMarketing.Services/Helpers/GetFile/IFileService.cs
@@ -0,0 +1,7 @@
+﻿namespace DigitalMarketing.DigitalMarketing.Services.Helpers.GetFile
+{
+    public interface IFileService
+    {
+        Task<string> UploadImageAsync(IFormFile file, string folderName);
+    }
+}
