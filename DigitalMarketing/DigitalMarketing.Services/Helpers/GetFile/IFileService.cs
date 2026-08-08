b34b3a1665ad4a4ab74d316f132b0e1c2345aa8d
“Taghi” <mohamadvarda9@gmail.com>
2026-08-08 17:08:40 +0330

    Delete IFileService&FileService for dublicate with othe file service

--- DigitalMarketing/DigitalMarketing.Services/Helpers/GetFile/IFileService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DigitalMarketing.DigitalMarketing.Services.Helpers.GetFile
-{
-    public interface IFileService
-    {
-        Task<string> UploadImageAsync(IFormFile file, string folderName);
-        Task DeleteAsync(string path);
-    }
-}
