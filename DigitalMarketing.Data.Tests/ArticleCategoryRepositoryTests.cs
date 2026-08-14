b18ec9ffd30dd10b3704c14a9801edef4fde4c18
“Taghi” <mohamadvarda9@gmail.com>
2026-08-14 12:55:55 +0330

    Add test to ArticleCategoryRepositoryTests method name GetAllAsync_DoesNotReturnSoftDeletedCategories

---
+++ DigitalMarketing.Data.Tests/ArticleCategoryRepositoryTests.cs
@@ -0,0 +1,67 @@
+﻿using DigitalMarketing.DigitalMarketing.Core.Entities;
+using DigitalMarketing.DigitalMarketing.Data.Repositories;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalMarketing.Data.Tests
+{
+    public class ArticleCategoryRepositoryTests : IDisposable
+    {
+        private readonly TestDbContextFactory _factory;
+        private readonly ArticleCategoryRepository _sut;
+        public ArticleCategoryRepositoryTests()
+        {
+            _factory = new TestDbContextFactory();
+            _sut = new ArticleCategoryRepository(_factory.Context);
+        }
+
+
+
+
+        [Fact]
+        public async Task GetAllAsync_DoesNotReturnSoftDeletedCategories()
+        {
+            // Arrange
+            _factory.Context.ArticleCategories.Add(new ArticleCategory { Name = "حذف ‌شده", Slug = "حذف-شده", IsDeleted = true });
+            _factory.Context.ArticleCategories.Add(new ArticleCategory { Name = "فعال", Slug = "فعال" });
+            await _factory.Context.SaveChangesAsync();
+
+            // Act
+            var result = await _sut.GetAllAsync();
+
+            // Assert
+            result.Should().HaveCount(1);
+            result.Single().Name.Should().Be("فعال");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public void Dispose() => _factory.Dispose();
+    }
+}
