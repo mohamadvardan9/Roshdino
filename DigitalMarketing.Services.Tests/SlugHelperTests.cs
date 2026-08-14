0de5a97dbca41e4ba5d6e8784ba9c84e82552f25
“Taghi” <mohamadvarda9@gmail.com>
2026-08-14 14:09:03 +0330

    Add test to SlugHelperTests method name GenerateSlug_ProducesExpectedSlug

---
+++ DigitalMarketing.Services.Tests/SlugHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using DigitalMarketing.DigitalMarketing.Services.Helpers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalMarketing.Services.Tests
+{
+    public class SlugHelperTests
+    {
+        [Theory]
+        [InlineData("Gaming LapTop","gaming-laptop")]
+        [InlineData("لبتاپ ایسر","لبتاپ-ایسر")]
+        [InlineData("  چند   فاصله  ", "چند-فاصله")]
+        [InlineData("Product!!! 100%", "product-100")]
+        [InlineData("Multiple---Dashes", "multiple-dashes")]
+        [InlineData("UPPER CASE TITLE", "upper-case-title")]
+        public void GenerateSlug_ProducesExpectedSlug(string input, string expected)
+        {
+            var result = SlugHelper.GenerateSlug(input);
+            result.Should().Be(expected);
+        }
+    }
+}
