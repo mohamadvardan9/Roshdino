16255d6e3fbbcfbea83e24788c78cc914a238803
“Taghi” <mohamadvarda9@gmail.com>
2026-08-14 05:12:30 +0330

    Add test to ProductCategoryRepositoryTests method name GetByIdAsync_WhenSoftDeleted_ReturnsNull

--- DigitalMarketing.Data.Tests/ProductCategoryRepositoryTests.cs
+++ DigitalMarketing.Data.Tests/ProductCategoryRepositoryTests.cs
@@ -40,6 +40,21 @@ namespace DigitalMarketing.Data.Tests
 
 
 
+        [Fact]
+        public async Task GetByIdAsync_WhenSoftDeleted_ReturnsNull()
+        {
+            // Arrange
+            var category = new ProductCategory { Name = "حذفی", Slug = "حذقی", IsDeleted = true };
+            _factory.Context.ProductCategories.Add(category);
+            await _factory.Context.SaveChangesAsync();
+
+            // Act
+            var result = await _sut.GetByIdAsync(category.Id);
+
+            // Assert
+            result.Should().BeNull(); // Query Filter حتی رو GetById هم اعمال می‌شه
+        }
+
 
 
 
