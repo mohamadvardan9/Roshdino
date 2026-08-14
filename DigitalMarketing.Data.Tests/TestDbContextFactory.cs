3e830f11b87f091daf9d29bd8747767ffa434979
“Taghi” <mohamadvarda9@gmail.com>
2026-08-14 05:00:47 +0330

    Add SQLite test database factory

---
+++ DigitalMarketing.Data.Tests/TestDbContextFactory.cs
@@ -0,0 +1,40 @@
+﻿using DigitalMarketing.DigitalMarketing.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalMarketing.Data.Tests
+{
+
+    /// <summary>
+    /// یه Base Class مشترک برای راه اندازی SQLite
+    /// (جلوگیری از تکرار کد)
+    /// </summary>
+    public class TestDbContextFactory : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+        public MyDbContext Context { get; }
+
+        public TestDbContextFactory()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+            var options = new DbContextOptionsBuilder<MyDbContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            Context = new MyDbContext(options);
+            Context.Database.EnsureCreated();
+        }
+
+
+        public void Dispose()
+        {
+            Context.Dispose();
+            _connection.Dispose();
+        }
+    }
+}
