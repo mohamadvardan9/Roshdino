21c8fa1d405c9217b23015aa5f814f7bdcb72735
“Taghi” <mohamadvarda9@gmail.com>
2026-08-15 13:34:57 +0330

    Add IContactMessageReposiory,ContactMessageRepository

---
+++ DigitalMarketing.Core.New/DigitalMarketing.Core/Interfaces/IContactMessageRepository.cs
@@ -0,0 +1,56 @@
+﻿using DigitalMarketing.Core.DigitalMarketing.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalMarketing.Core.DigitalMarketing.Core.Interfaces
+{
+    public interface IContactMessageRepository
+    {
+        /// <summary>
+        /// Retrieves all contactMessages
+        /// </summary>
+        /// <returns>
+        /// The task result contains a list of all contactMessages ordered by createdAt date descending
+        /// </returns>>
+        Task<List<ContactMessage>> GetAllAsync();
+        /// <summary>
+        /// Retrieves a contactMessage by its unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the contactMessage.</param>
+        /// <returns>
+        /// The task result contains the contactMessage with its related data if found; otherwise, null.
+        /// </returns>
+        Task<ContactMessage?> GetByIdAsync(int id);
+
+
+
+
+
+
+        Task AddAsync(ContactMessage message);
+        // <summary>
+        /// Marks the specified contact message as read
+        /// and updates it in the data context.
+        /// </summary>
+        /// <param name="message">
+        /// The contact message to mark as read.
+        /// </param>
+        void MarkAsRead(ContactMessage message);
+        void Delete(ContactMessage message);
+
+
+
+
+
+        /// <summary>
+        /// Gets the total number of unread contact messages.
+        /// </summary>
+        /// <returns>
+        /// The number of contact messages that have not been marked as read.
+        /// </returns>
+        Task<int> GetUnreadCountAsync();
+        Task SaveChangesAsync();
+
+    }
+}
---
+++ DigitalMarketing.Data.New/DigitalMarketing.Data/Repositories/ContactMessageRepository.cs
@@ -0,0 +1,62 @@
+﻿using DigitalMarketing.Core.DigitalMarketing.Core.Entities;
+using DigitalMarketing.Core.DigitalMarketing.Core.Interfaces;
+using DigitalMarketing.DigitalMarketing.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalMarketing.Data.DigitalMarketing.Data.Repositories
+{
+    public class ContactMessageRepository : IContactMessageRepository
+    {
+        private readonly MyDbContext _context;
+        public ContactMessageRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+
+        public async Task<List<ContactMessage>> GetAllAsync()
+            => await _context.ContactMessages
+            .OrderByDescending(cm => cm.CreatedAt)
+            .ToListAsync();
+
+        public async Task<ContactMessage?> GetByIdAsync(int id)
+            => await _context.ContactMessages
+            .FirstOrDefaultAsync(cm => cm.Id == id);
+
+
+
+
+        public async Task AddAsync(ContactMessage message) => await _context.ContactMessages.AddAsync(message);
+
+        public void MarkAsRead(ContactMessage message)
+        {
+            message.IsRead = true;
+            _context.ContactMessages.Update(message);
+        }
+
+        public void Delete(ContactMessage message)
+        {
+            message.IsDeleted = true;
+            _context.ContactMessages.Update(message);
+        }
+
+
+
+
+        public async Task<int> GetUnreadCountAsync()
+            => await _context.ContactMessages
+            .CountAsync(cm => !cm.IsRead);
+
+
+
+        public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+
+        
+    }
+}
