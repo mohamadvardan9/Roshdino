cdb5809032625bae3f842d60a57168232021f17d
“Taghi” <mohamadvarda9@gmail.com>
2026-08-15 08:06:13 +0330

    Add ArticleController to DigitalMarketing.Web with Views,ViewModels, . . .

---
+++ DigitalMarketing.Web/Controllers/ArticleController.cs
@@ -0,0 +1,56 @@
+﻿using DigitalMarketing.DigitalMarketing.Services.Interfaces;
+using DigitalMarketing.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalMarketing.Web.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly IArticleService _articleService;
+        private readonly IArticleCategoryService _articleCategoryService;
+        public ArticleController(IArticleService articleService, IArticleCategoryService articleCategoryService)
+        {
+            _articleService = articleService;
+            _articleCategoryService = articleCategoryService;
+        }
+
+
+
+        // GET: /articles
+        // GET: /articles?categoryId=2
+        [Route("articles")]
+        public async Task<IActionResult> Index(int? categoryId)
+        {
+            var articles = categoryId.HasValue
+                ? await _articleService.GetByCategoryAsync(categoryId.Value)
+                : await _articleService.GetPublishedAsync();
+
+            var categories = await _articleCategoryService.GetAllAsync();
+
+            var viewModel = new ArticleListViewModel
+            {
+                Articles = articles,
+                Categories = categories,
+                SelectedCategoryId = categoryId
+            };
+
+            return View(viewModel);
+        }
+
+
+
+
+
+
+        // GET: /articles/{slug}
+        [Route("articles/{slug}")]
+        public async Task <IActionResult> Details(string slug)
+        {
+            var article = await _articleService.GetBySlugAsync(slug);
+
+            if (article == null) return NotFound();
+
+            return View(article);
+        }
+    }
+}
---
+++ DigitalMarketing.Web/Models/ArticleListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DigitalMarketing.DigitalMarketing.Services.DTOs.ArticleCategoryDtos;
+using DigitalMarketing.DigitalMarketing.Services.DTOs.ArticleDtos;
+
+namespace DigitalMarketing.Web.Models
+{
+    public class ArticleListViewModel
+    {
+        public List<ArticleDto> Articles { get; set; } = new();
+        public List<ArticleCategoryDto> Categories { get; set; } = new();
+        public int? SelectedCategoryId { get; set; }
+    }
+}
