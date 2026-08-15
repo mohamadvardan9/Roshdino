a0b6dca11601ac90cb1864f395b4c5a8bf96fd9c
“Taghi” <mohamadvarda9@gmail.com>
2026-08-15 07:31:43 +0330

    Add _CTA.cshtml

---
+++ DigitalMarketing.Web/Views/Home/_CTA.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DigitalMarketing.Web.Views.Home
+{
+    public class _CTAModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
