0f1a56456d117745e0d6889bb9ebc2d3d2c8dcc7
“Taghi” <mohamadvarda9@gmail.com>
2026-08-15 07:28:41 +0330

    Add _Portfolio.cshtml

---
+++ DigitalMarketing.Web/Views/Home/_Portfolio.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DigitalMarketing.Web.Views.Home
+{
+    public class _PortfolioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
