2d731e1fa04360759b39625663af082c4a47d043
“Taghi” <mohamadvarda9@gmail.com>
2026-08-16 19:48:19 +0330

    Add some information about site to MainController , add MainViewModel to Main/Index.cshtml and Edit view

--- DigitalMarketing.Admin/Controllers/MainController.cs
+++ DigitalMarketing.Admin/Controllers/MainController.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DigitalMarketing.Admin.Models;
+using DigitalMarketing.DigitalMarketing.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DigitalMarketing.Admin.Controllers
 {
     public class MainController : Controller
     {
+        private readonly MyDbContext _context;
+        public MainController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+
 
         public IActionResult Index()
         {
-            return View();
+            MainViewModel model = new MainViewModel
+            {
+                ArticlesCount = _context.Articles.Count(),
+                ProductsCount = _context.Products.Count(),
+                ArticlesGrowth = 12.5
+            };
+
+
+            return View(model);
         }
     }
 }
---
+++ DigitalMarketing.Admin/Models/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DigitalMarketing.Admin.Models
+{
+    public class MainViewModel
+    {
+        public int ArticlesCount { get; set; }
+        public int ProductsCount { get; set; }
+        public double ArticlesGrowth { get; set; }
+    }
+}
