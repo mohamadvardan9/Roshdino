776b83b9c607aa407036346771d215b2b674863c
“Taghi” <mohamadvarda9@gmail.com>
2026-08-20 12:48:10 +0330

    feat: create about us controller and views

---
+++ DigitalMarketing.Web/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalMarketing.Web.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DigitalMarketing.Web/Views/AboutUs/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DigitalMarketing.Web.Views.AboutUs
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
