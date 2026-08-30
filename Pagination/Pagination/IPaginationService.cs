3a7ebb5461e493e8ed58281f488d78189029f6c1
“Taghi” <mohamadvarda9@gmail.com>
2026-08-31 02:44:31 +0330

    feat: improve internal linking for _Services.cshtml

--- Pagination/Pagination/IPaginationService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DigitalMarketing.Web.Pagination
-{
-    public interface IPaginationService
-    {
-        Task<PaginationResponse<T>> PaginateAsync<T>(IQueryable<T> query, PaginationRequest request,
-            CancellationToken cancellationToken = default);
-    }
-}
--- Pagination/Pagination/PaginationExtensions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DigitalMarketing.Web.Pagination
-{
-    public class PaginationExtensions
-    {
-    }
-}
